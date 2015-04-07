8cb55b4a96705e6ecb1cc93fd0b2133ebbadba79
Noel Delgado <auhcsei@gmail.com>
2015-04-07 13:20:58 -0500

    add / webpack loader for autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -15,14 +15,14 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader!autoprefixer-loader?browsers=last 2 version' },
       { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&minetype=application/font-woff" },
       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&minetype=application/font-woff" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,     loader: "url?limit=10000&minetype=application/octet-stream" },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,     loader: "file" },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,     loader: "url?limit=10000&minetype=image/svg+xml" },
       { test: /\.(png|jpg)$/,                   loader: 'url-loader?limit=8192'}, // inline base64 URLs for <=8k images, direct URLs for the rest
-      { test: /\.less$/,                        loader: "style-loader!css-loader!less-loader"}
+      { test: /\.less$/,                        loader: "style-loader!css-loader!autoprefixer-loader?browsers=last 2 version!less-loader"}
     ]
   },
   plugins: [
