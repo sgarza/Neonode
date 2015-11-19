5728843d11bd17f82c042319345b91f390d9973e
Sergio de la Garza <sergio@delagarza.io>
2015-11-18 22:46:27 -0600

    Fix CSFError middleware Handle ForbiddenError in Error middleware

--- middlewares/CSRFError.js
+++ middlewares/CSRFError.js
@@ -9,10 +9,6 @@ if (CONFIG.sessions === false) {
 logger.log("Setting error handler for CSRF");
 
 module.exports = function (err, req, res, next) {
-  logger.error('CSRF', err, res.locals._csrf)
-  if (err.code !== 'EBADCSRFTOKEN') return next(err)
-
-  // handle CSRF token errors here
-  res.status(403).send(err);
-  res.send('session has expired or form tampered with');
+  logger.error('CSRF', err, res.locals._csrf);
+  next(err);
 };
--- middlewares/Error.js
+++ middlewares/Error.js
@@ -1,9 +1,13 @@
 module.exports = function(err, req, res, next) {
   logger.error(err.stack);
   if (err.name && err.name === 'NotFoundError') {
-    return res.status(404).render('shared/404.html', {message : err.message});
+    return res.status(404).render('shared/404.html', { message : err.message });
+  }
+
+  if (err.name && err.name === 'ForbiddenError') {
+    return res.status(403).render('shared/403.html', { message : err.message });
   }
 
   res.status(500);
-  res.render('shared/500.html', {layout : false, error : err.stack})
+  res.render('shared/500.html', {layout : false, error : err.stack});
 }
